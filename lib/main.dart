332139f3a7a1548c11958ba9f7ab489c70dcc431
im-endangered <im-endangered>
2022-04-10 09:34:02 +0530

    widget created for homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import './homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -26,103 +27,9 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // actions: [
-        //   IconButton(onPressed: () {}, icon: Icon(Icons.percent)),
-        // ],
         title: Text("appLotita-Simplifying your lifestyle"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            alignment: Alignment.topCenter,
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(9),
-                  child: Text(
-                    DateFormat.yMMMMd().format(DateTime.now()),
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
-                StreamBuilder(
-                  stream: Stream.periodic(const Duration(seconds: 1)),
-                  builder: (context, snapshot) {
-                    return Center(
-                      child: Text(
-                        DateFormat('hh:mm:ss').format(DateTime.now()),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 150,
-          ),
-          Container(
-            alignment: Alignment.center,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.all(20),
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.money,
-                          color: Colors.green,
-                          size: 50,
-                        ),
-                      ),
-                    ),
-                    Text('Expense Manager'),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.all(20),
-                      child: IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.percent_outlined,
-                            color: Colors.red,
-                            size: 50,
-                          )),
-                    ),
-                    Text('Split Bills'),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.all(20),
-                      child: IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.note,
-                            color: Colors.blue,
-                            size: 50,
-                          )),
-                    ),
-                    Text('Notes'),
-                  ],
-                )
-              ],
-            ),
-          ),
-        ],
-      ),
+      body: HomePage(),
     );
   }
 }
